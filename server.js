0dd59ff7bb41b37464c98b490e9ab32d434cc262
Dwightu <yudehui1115@gmail.com>
2022-07-20 23:50:34 -0400

    Update

--- server.js
+++ server.js
@@ -135,6 +135,9 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
+app.get('/hello', (req, res) => {
+    res.send('hello world')
+})
 
 app.listen(5000, () => {
     console.log('Server is Running')
